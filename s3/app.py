844e94635ddc7cf509b7b692e153e522320ad409
xla344 <xla344@sfu.ca>
2022-03-19 09:09:34 -0700

    implement playlist update for server

--- s3/app.py
+++ s3/app.py
@@ -62,15 +62,89 @@ def readiness():
     return Response("", status=200, mimetype="application/json")
 
 
-@bp.route('add_songs/<playlist_id>', methods=['PUT'])
+@bp.route('add_songs/<playlist_id>', methods=['PUT', 'GET'])
 def add_songs_to_playlist(playlist_id):
-    raise NotImplemented
-
+    headers = request.headers
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        music = content['Music']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    if response.status_code != 200:
+        response = {
+            "Count": 0,
+            "Items": []
+         }
+        return app.make_response((response, 404))
+    item = response.json()['Items']
+    music_list = (item['songs']if 'songs' in item else None)
+    if music_list is None:
+        music_list = {music}
+    else:
+        music_list.add(music)
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params=payload,
+        json={"songs": music_list}
+        headers={'Authorization': headers['Authorization']})
+    return(response.json())
 
 @bp.route('delete_songs/<playlist_id>', methods=['PUT'])
 def delete_songs_to_playlist(playlist_id):
-    raise NotImplemented
-
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        music = content['Music']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    payload = {"objtype": "playlist", "objkey": playlist_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']})
+    if response.status_code != 200:
+        response = {
+            "Count": 0,
+            "Items": []
+         }
+        return app.make_response((response, 404))
+    item = response.json()['Items']
+    music_list = (item['songs']if 'songs' in item else None)
+    if music_list is None or music not in music_list:
+        response = {
+            "Count": 0,
+            "Items": []
+        }
+        return app.make_response((response, 404))
+    else:
+        music_list.discard(music)
+        # check if the set is empty 
+        if not music_list:
+            music_list = None
+        url = db['name'] + '/' + db['endpoint'][3]
+        response = requests.put(
+            url,
+            params=payload,
+            json={"songs": music_list}
+            headers={'Authorization': headers['Authorization']})
+        return(response.json())
 
 @bp.route('/', methods=['POST'])
 def create_playlist():
