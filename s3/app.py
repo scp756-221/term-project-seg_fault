09a9dd05f1ba0cbd4a7631770fd684e49b5c826b
eamonn-zh <eamonn0223@gmail.com>
2022-03-19 00:10:46 -0700

    add s3 template

--- s3/app.py
+++ s3/app.py
@@ -1,6 +1,6 @@
 """
 SFU CMPT 756
-Sample application---user service.
+Sample application---playlist service.
 """
 
 # Standard library modules
@@ -27,7 +27,7 @@ import simplejson as json
 app = Flask(__name__)
 
 metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'User process')
+metrics.info('app_info', 'Playlist process')
 
 bp = Blueprint('app', __name__)
 
@@ -62,113 +62,35 @@ def readiness():
     return Response("", status=200, mimetype="application/json")
 
 
-@bp.route('/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}), status=401,
-                        mimetype='application/json')
-    try:
-        content = request.get_json()
-        email = content['email']
-        fname = content['fname']
-        lname = content['lname']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
-    url = db['name'] + '/' + db['endpoint'][3]
-    response = requests.put(
-        url,
-        params={"objtype": "user", "objkey": user_id},
-        json={"email": email, "fname": fname, "lname": lname})
-    return (response.json())
+@bp.route('add_songs/<playlist_id>', methods=['PUT'])
+def add_songs_to_playlist(playlist_id):
+    raise NotImplemented
+
+
+@bp.route('delete_songs/<playlist_id>', methods=['PUT'])
+def delete_songs_to_playlist(playlist_id):
+    raise NotImplemented
 
 
 @bp.route('/', methods=['POST'])
-def create_user():
-    """
-    Create a user.
-    If a record already exists with the same fname, lname, and email,
-    the old UUID is replaced with a new one.
-    """
-    try:
-        content = request.get_json()
-        lname = content['lname']
-        email = content['email']
-        fname = content['fname']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
-    url = db['name'] + '/' + db['endpoint'][1]
-    response = requests.post(
-        url,
-        json={"objtype": "user",
-              "lname": lname,
-              "email": email,
-              "fname": fname})
-    return (response.json())
+def create_playlist():
+    raise NotImplemented
 
 
-@bp.route('/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}),
-                        status=401,
-                        mimetype='application/json')
-    url = db['name'] + '/' + db['endpoint'][2]
-
-    response = requests.delete(url,
-                               params={"objtype": "user", "objkey": user_id})
-    return (response.json())
+@bp.route('/<playlist_id>', methods=['DELETE'])
+def delete_playlist(user_id):
+    raise NotImplemented
 
 
-@bp.route('/<user_id>', methods=['GET'])
-def get_user(user_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
-        return Response(
-            json.dumps({"error": "missing auth"}),
-            status=401,
-            mimetype='application/json')
-    payload = {"objtype": "user", "objkey": user_id}
-    url = db['name'] + '/' + db['endpoint'][0]
-    response = requests.get(url, params=payload)
-    return (response.json())
-
-
-@bp.route('/login', methods=['PUT'])
-def login():
-    try:
-        content = request.get_json()
-        uid = content['uid']
-    except Exception:
-        return json.dumps({"message": "error reading parameters"})
-    url = db['name'] + '/' + db['endpoint'][0]
-    response = requests.get(url, params={"objtype": "user", "objkey": uid})
-    data = response.json()
-    if len(data['Items']) > 0:
-        encoded = jwt.encode({'user_id': uid, 'time': time.time()},
-                             'secret',
-                             algorithm='HS256')
-    return encoded
-
-
-@bp.route('/logoff', methods=['PUT'])
-def logoff():
-    try:
-        content = request.get_json()
-        _ = content['jwt']
-    except Exception:
-        return json.dumps({"message": "error reading parameters"})
-    return {}
+@bp.route('/<playlist_id>', methods=['GET'])
+def get_playlist(playlist_id):
+    raise NotImplemented
 
 
 # All database calls will have this prefix.  Prometheus metric
 # calls will not---they will have route '/metrics'.  This is
 # the conventional organization.
-app.register_blueprint(bp, url_prefix='/api/v1/user/')
+app.register_blueprint(bp, url_prefix='/api/v1/playlist/')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
