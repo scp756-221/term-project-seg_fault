efb92e2cbd6de0744cd189a2c54d563b2ea61ea2
eamonn-zh <eamonn0223@gmail.com>
2022-04-10 15:16:55 -0700

    update gatling script

--- gatling-playlist.sh
+++ gatling-playlist.sh
@@ -3,8 +3,8 @@ docker container run --rm \
   -v ${PWD}/gatling/results:/opt/gatling/results \
   -v ${PWD}/gatling:/opt/gatling/user-files \
   -v ${PWD}/gatling/target:/opt/gatling/target \
-  -e CLUSTER_IP=aa05a4b3793ed490aa731247ad59c78c-446087025.us-west-2.elb.amazonaws.com \
-  -e USERS=10 \
+  -e CLUSTER_IP=a46554ebb03344aa7be0a794f8cdde78-213372141.us-west-2.elb.amazonaws.com \
+  -e USERS=$1 \
   -e SIM_NAME=ReadPlaylistSim \
   --label gatling \
   ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
