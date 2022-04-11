521492456eb9a04d588c438edfa97a71930f079c
Yiming Zhang <eamonn0223@gmail.com>
2022-04-11 16:02:31 -0700

    fix a minor bug

--- gatling-playlist.sh
+++ gatling-playlist.sh
@@ -3,7 +3,7 @@ docker container run --rm \
   -v ${PWD}/gatling/results:/opt/gatling/results \
   -v ${PWD}/gatling:/opt/gatling/user-files \
   -v ${PWD}/gatling/target:/opt/gatling/target \
-  -e CLUSTER_IP=a46554ebb03344aa7be0a794f8cdde78-213372141.us-west-2.elb.amazonaws.com \
+  -e CLUSTER_IP=ab7efae5d424241c38c65d976a9f305b-1433551554.us-west-2.elb.amazonaws.com \
   -e USERS=$1 \
   -e SIM_NAME=ReadPlaylistSim \
   --label gatling \
